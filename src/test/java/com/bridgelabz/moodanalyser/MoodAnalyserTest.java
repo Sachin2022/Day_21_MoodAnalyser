1a6690b6d1c1e52d9e5f0200b91447d86778ff00
Sachin2022 <sachikatarki1995@gmail.com>
2022-03-03 12:50:54 +0530

    Added:Code to analyse Mood message Should Return Sad in Test Case

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -2,13 +2,15 @@ package com.bridgelabz.moodanalyser;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
 
 	@Test
-	public void shouldAnswerWithTrue() {
-		assertTrue(true);
+	public void givenMessage_WhenProper_ShouldReturnSad() {
+		MoodAnalyser moodanalyser = new MoodAnalyser();
+		String actualResult = moodanalyser.analyseMood("User is Sad");
+		Assert.assertEquals("Sad", actualResult);
 	}
-
 }
